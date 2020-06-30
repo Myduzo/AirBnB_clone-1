5ce332827a6102d721db72c468a3fe2719e6c8b2
Myduzo <sahli.youssef@outlook.com>
2020-06-30 11:07:02 +0100

    pep8 fixed

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,41 +6,42 @@ import uuid
 import os
 import datetime
 
+
 class TestBaseModel(unittest.TestCase):
-	"""Tests for the BaseModel class"""
+    """Tests for the BaseModel class"""
 
-	def test_id(self):
-		"""Test id"""
-		test1 = BaseModel()
-		test2 = BaseModel()
-		self.assertNotEqual(test1.id, test2.id)
+    def test_id(self):
+        """Test id"""
+        test1 = BaseModel()
+        test2 = BaseModel()
+        self.assertNotEqual(test1.id, test2.id)
 
-	def test_str(self):
-		"""Test string"""
-		test = BaseModel()
-		self.assertIsInstance(test.__str__(), str)
+    def test_str(self):
+        """Test string"""
+        test = BaseModel()
+        self.assertIsInstance(test.__str__(), str)
 
-	def test_save(self):
-		"""Test save"""
-		self.assertFalse(os.path.exists("file.json"))
+    def test_save(self):
+        """Test save"""
+        self.assertFalse(os.path.exists("file.json"))
 
-	def test_to_dict(self):
-		"""Test to dict"""
-		test1 = BaseModel()
-		self.assertIsInstance(test1.to_dict(), dict)
-	
-	def testInit(self):
-		"""Test init"""
-		test1 = BaseModel()
-		test1.my_num = 89
-		test1.my_name = "Hol"
-		test2 = BaseModel(**test1.to_dict())
+    def test_to_dict(self):
+        """Test to dict"""
+        test1 = BaseModel()
+        self.assertIsInstance(test1.to_dict(), dict)
 
-		self.assertEqual(test1.id, test2.id)
-		self.assertEqual(test1.created_at, test2.created_at)
-		self.assertEqual(test1.updated_at, test2.updated_at)
-		self.assertEqual(test2.my_num, 89)
-		self.assertEqual(test2.my_name, "Hol")
+    def testInit(self):
+        """Test init"""
+        test1 = BaseModel()
+        test1.my_num = 89
+        test1.my_name = "Hol"
+        test2 = BaseModel(**test1.to_dict())
+
+        self.assertEqual(test1.id, test2.id)
+        self.assertEqual(test1.created_at, test2.created_at)
+        self.assertEqual(test1.updated_at, test2.updated_at)
+        self.assertEqual(test2.my_num, 89)
+        self.assertEqual(test2.my_name, "Hol")
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
