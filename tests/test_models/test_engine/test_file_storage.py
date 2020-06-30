c5d54b93aa747a948df6532b067cb292a707a0d3
Myduzo <sahli.youssef@outlook.com>
2020-06-30 11:07:49 +0100

    pep8 fixed

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,37 +9,37 @@ import datetime
 
 
 class TestFileStorage(unittest.TestCase):
-	"""Tests for the FileStorage class"""
+    """Tests for the FileStorage class"""
 
-	def test_objects(self):
-		"""Test objects"""
-		test = FileStorage()
-		self.assertIsInstance(test._FileStorage__objects, dict)
+    def test_objects(self):
+        """Test objects"""
+        test = FileStorage()
+        self.assertIsInstance(test._FileStorage__objects, dict)
 
-	def test_file_path(self):
-		""" Check file_path type """
-		test = FileStorage()
-		self.assertIsInstance(test._FileStorage__file_path, str)
+    def test_file_path(self):
+        """ Check file_path type """
+        test = FileStorage()
+        self.assertIsInstance(test._FileStorage__file_path, str)
 
-	def test_file_exist(self):
-		"""Test exist"""
-		self.assertFalse(os.path.exists("file.json"))
+    def test_file_exist(self):
+        """Test exist"""
+        self.assertFalse(os.path.exists("file.json"))
 
-	def test_all(self):
-		"""Test all"""
-		my_dict = {"Base01": {"foo1": 62, "foo2": 19, "foo3": 2},
-				   "Base02": {"foo4": 30, "foo5": 89, "foo6": 56}}
-		FileStorage._FileStorage__objects = my_dict
-		self.assertEqual(my_dict, FileStorage().all())
+    def test_all(self):
+        """Test all"""
+        my_dict = {"Base01": {"foo1": 62, "foo2": 19, "foo3": 2},
+                   "Base02": {"foo4": 30, "foo5": 89, "foo6": 56}}
+        FileStorage._FileStorage__objects = my_dict
+        self.assertEqual(my_dict, FileStorage().all())
 
-	def test_new(self):
-		"""Test new"""
-		test = BaseModel()
-		my_class = FileStorage()
-		my_class.new(test)
+    def test_new(self):
+        """Test new"""
+        test = BaseModel()
+        my_class = FileStorage()
+        my_class.new(test)
 
-		self.assertIs(type(my_class.all()), dict)
-		self.assertEqual(my_class.all()["BaseModel" + "." + test.id], test)
-	
+        self.assertIs(type(my_class.all()), dict)
+        self.assertEqual(my_class.all()["BaseModel" + "." + test.id], test)
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
